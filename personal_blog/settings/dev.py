aa8ea67ef21319e5aaa180ae06f4a37fea1e5491
pguyard <pguyard@gmail.com>
2020-12-19 18:06:32 +0000

    Moved scret key to .env

--- personal_blog/settings/dev.py
+++ personal_blog/settings/dev.py
@@ -3,9 +3,6 @@ from .base import *
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '6zzpp9!in+h(l@zh^vs+f$!$d6lu03l3^eol9aveg!njp32b38'
-
 # SECURITY WARNING: define the correct hosts in production!
 ALLOWED_HOSTS = ['*'] 
 
